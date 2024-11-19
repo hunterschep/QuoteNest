746c535fd06e81f5b64b89d6b3294471b4f820b6
Hunter S <67876717+hunterschep@users.noreply.github.com>
2024-11-19 16:48:11 -0500

    Initial Commit

---
+++ QuoteNest/QuoteNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteNestApp.swift
+//  QuoteNest
+//
+//  Created by Hunter Scheppat on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct QuoteNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
